8f02a262e5bfe313c0a673ccee86ca874225135b
small.coder@foxmail.com <small.coder@foxmail.com>
2018-06-25 11:21:09 +0800

    commit codes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+// import 'antd/dist/antd.css';
+import './App.css';
+class App extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>hello world</h1>
+    );
+  }
+}
+
+export default App;
